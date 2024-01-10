001695a500ce87e41d63faf57dd9d88024c85ee8
KotovsAndrejs <125635760+KotovsAndrejs@users.noreply.github.com>
2024-01-10 13:21:09 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -6,37 +6,51 @@
 # 
 
 def add(list, item):
-  pass
+  list.append(item)
 
 
 def remove(list, index):
+  list.pop(index)
   pass
 
 
 def clear(list):
+  list.clear()
   pass
 
 
 def print_list(list):
+  for i in range(len(list)):    
+    print(i, " ", list[i])
   pass
 
 
 list = []
-print("List is empty now, what you want to do?")
+print("List of guests is empty now, what you want to do?")
 while True:
-  choice = int(input("1. Add\n2. Remove\n3. Clear\n4. Print list\n"))
+  choice = int(input("1. Add a guest\n2. Remove a guest\n3. Clear the list\n4. Print the list\n"))
   if choice == 1:
     item = input("What you want to add?\n")
     add(list, item)
     print_list(list)
   elif choice == 2:
-    index = int(input("What you want to remove?\n"))
-    remove(list, index)
-    print_list(list)
+    if not list:
+      print("There is nothing to remove in the list\n")
+    else:
+      index = int(input("What you want to remove?\n"))
+      remove(list, index)
+      print_list(list)
   elif choice == 3:
-    clear(list)
-    print_list(list)
+    if not list:
+      print("The list is already clear\n")
+    else:
+      clear(list)
+      print_list(list)
   elif choice == 4:
-    print_list(list)
+    if not list:
+      print("There is nothing in list\n")
+    else:
+      print_list(list)
   else:
     print("Invalid input")
+
